f3ef906a6b6187190401c0b330e78b33eae3a473
Alvi Ahmed <alviahmed12@gmail.com>
2020-02-24 01:22:39 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+const express = require('express');
+const bodyParser = require('body-parser')
+const app =express();
+//const port = 5000;
+var http = require('http');
+var server = http.Server(app);
+
+var PORT = process.env.PORT || 5000;
+
+const sessionId = uuid.v4();
+
+app.use(bodyParser.urlencoded({
+  extended:false
+}))
+
+app.use(express.static('isp-bot'));
+
+
+app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true); 
+
+    next();
+}
+
+);
+//app.get('/', function(request, response){
+//response.sendFile(__dirname + "/isp-bot/index.html");
+//});
+app.post('/send-msg',(req,res)=>{
+    runSample(req.body.MSG).then(data=>{
+        res.send({Reply:data})
+    })
+})
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'isp-bot-joksap') {
+  // A unique identifier for the given session
+  
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename:"/Users/alviahmed/Downloads/Isp_bot/ISP-bot-0d91bde012a3.json"
+  });
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  return result.fulfillmentText; 
+}
+
+app.listen(process.env.PORT || 5000,function(){
+  console.log("Server is running on port 5000.");
+});
